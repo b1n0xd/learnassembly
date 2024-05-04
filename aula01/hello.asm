bd49489a45f09668497bdd2b89235ea5492f52c2
b1n0xd <lucianovieiradev@gmail.com>
2024-05-04 15:55:03 -0300

    include space after msg.

--- aula01/hello.asm
+++ aula01/hello.asm
@@ -1,12 +1,19 @@
 ; nasm -f elf64 hello.asm
+; Linkeditar - Transformar o programa em linguagem de máquina para um exec.
 
 section .data
-
+    msg db 'Hello World!', 0xA
+    tam equ $-msg
 section .text
 
 global _start
 
 _start:
+    mov EAX, 0x4
+    mov EBX, 0x1
+    mov ECX, msg
+    mov EDX, tam
+    int 0x80
     mov EAX, 0x1
     mov RBX, 0x0
     int 0x80
